e586e27a6529de34839126e8ebb6bdd47d037d92
Mattia Mascia <mmascia@redhat.com>
2018-10-29 16:50:11 +0100

    fix annotation name

--- webhook/webhook.go
+++ webhook/webhook.go
@@ -25,7 +25,7 @@ var (
 
 	annotationRegistry = []*registeredAnnotation{
 		{"sidecar.agent.vaultproject.io/inject", alwaysValidFunc},
-		{"sidecar.agent..vaultproject.io/status", alwaysValidFunc},
+		{"sidecar.agent.vaultproject.io/status", alwaysValidFunc},
 	}
 
 	annotationPolicy = annotationRegistry[0]
